4a385f07d4ccb67144e64949bcb4be77f5b71c03
Alexander Simisi <smsxander@gmail.com>
2020-02-27 00:17:34 +0100

    cryptage de mot de passe avec bcrypt-nodejs

--- models/user.js
+++ models/user.js
@@ -1,10 +1,27 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
+const bcrypt = require ('bcrypt-nodejs')
 
 //création de model
 const userSchema = new Schema({
     email: {type: String, unique: true, lowercase: true},
     password: {type: String}
+})
+
+userSchema.pre("save", function(next){
+    const user = this;
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) {
+            return next(err)
+        }
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if(err) {
+                return next(err)
+            }
+            user.password = hash;
+            next()
+        })
+    })
 })
 
 const UserModel = mongoose.model("user", userSchema);
