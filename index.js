a708734f667620f2b8f7bcba5884d09f5d280ce9
Alexander Simisi <smsxander@gmail.com>
2020-02-26 19:45:17 +0100

    Connexion à la base de données

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const morgan = require("morgan");
 const router = require("./routes");
 const http = require("http");
 const expressServer = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://alex:170608@cluster0-cwezd.mongodb.net/test?retryWrites=true&w=majority", 
+{ 
+    useUnifiedTopology: true,
+    useNewUrlParser: true 
+})
+mongoose.connection
+    .once("open", ()=> console.log("Connecté à Mlab"))
+    .on("error", error => console.log(`Erreur de connexion à Mlab ${error}`))
 
 expressServer.use(morgan("combined"))
 expressServer.use(bodyParser.json({type: '*/'}));
