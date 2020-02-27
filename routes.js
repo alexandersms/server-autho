36e62697c1609ce06ae508a6c779ba16dae399f2
Alexander Simisi <smsxander@gmail.com>
2020-02-27 02:29:56 +0100

    strategie de login

--- routes.js
+++ routes.js
@@ -2,11 +2,16 @@ const AuthentificationController = require('./controllers/authentification');
 require("./services/passport")
 const passport = require("passport");
 
-const requireToken = passport.authenticate("jwt", { session: false })
+const requireToken = passport.authenticate("jwt", { session: false });
+const requireValidCredentials = passport.authenticate("local", {session: false})
+
 module.exports = function (expressServer) {
-    expressServer.post("/signup", AuthentificationController.signup)
+    expressServer.post("/signup", AuthentificationController.signup);
+
     expressServer.get("/ressources", requireToken, (req, res) => {
         res.send({codeRessource: 42})
-    })
+    });
+
+    expressServer.post("/signin", requireValidCredentials, AuthentificationController.signin)
 };
 
--- services/passport.js
+++ services/passport.js
@@ -1,26 +1,50 @@
-const passport = require("passport")
+const passport = require("passport");
 const User = require("../models/user");
 const config = require("../config");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const localStrategy = require("passport-local");
 
 const jwtOptions = {
-    jwtFromRequest: ExtractJwt.fromHeader("authorization"),
-    secretOrKey: config.secret
+  jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+  secretOrKey: config.secret
 };
 
-const jwtLogin = new JwtStrategy(jwtOptions, (payload, done)=> {
-    const userId = payload.sub;
-    User.findById(userId, (err, user) => {
-        if(err){
-            return done(err, false)
-        }
-        if (user) {
-            return done(null, user)
-        } else {
-            return done(null, false)
-        }
-    })
-})
+const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
+  const userId = payload.sub;
+  User.findById(userId, (err, user) => {
+    if (err) {
+      return done(err, false);
+    }
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  });
+});
 
-passport.use(jwtLogin)
+const localOptions = { usernameField: "email" };
+const localLoginStrategy = new localStrategy(
+  localOptions,
+  (email, password, done) => {
+    User.findOne({ email }, (err, user) => {
+      if (err) return done(err);
+      if (!err) return done(null, false);
+      user.isPasswordEqualTo(password, (err, isMatch) => {
+        if (err) {
+          return done(err);
+        }
+
+        if (!isMatch) {
+          return done(null, false);
+        } else {
+          return done(null, user);
+        }
+      });
+    });
+  }
+);
+
+passport.use(jwtLogin);
+passport.use(localLoginStrategy);
